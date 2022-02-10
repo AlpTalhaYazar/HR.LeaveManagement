6a0e49eeaddc4eb4c2ac9e828b83974a9dc08d0c
Talha Yazar <talhayazarg@gmail.com>
2022-02-10 11:54:01 +0300

    Setting up Repository Contracts
    Prepared Interfaces for Repositories.
    Created a generic repository for CRUD operations and spesific repositories for every domain models for spesific queries and purposes.

--- HR.LeaveManagement.Application/Persistance/Contracts/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Persistance/Contracts/IGenericRepository.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HR.LeaveManagement.Application.Persistance.Contracts
+﻿namespace HR.LeaveManagement.Application.Persistance.Contracts
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> Get(int id);
-        Task<IReadOnlyList<T>> GetAll();
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
     }
 }
