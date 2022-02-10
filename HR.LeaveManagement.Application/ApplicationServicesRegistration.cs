4add84e51173a871e74899c8ba56c3596cc5d858
Talha Yazar <talhayazarg@gmail.com>
2022-02-10 12:49:03 +0300

    Setting up AutoMapper
    Created separate DTOs for domain models and a BaseDto for common properties.
    Installed Automapper package.
    Defined mappings between dtos and domain models.
    Created a class for dependency implementations.

---
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
